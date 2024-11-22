14e243c96bf76732e0fa0a42dc01eef43252ecb9
Lior Shor <LiorShor123@gmail.com>
2024-11-22 11:10:30 +0200

    Add recipe fetching with Combine to RecipesViewModel

--- CalAssignment/CalAssignment/Modules/Recipes/RecipesViewModel.swift
+++ CalAssignment/CalAssignment/Modules/Recipes/RecipesViewModel.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 class RecipesViewModel: ObservableObject {
+    @Published var recipes: [Recipe] = []
+    private let repository: RecipesRepository
+    private var cancellables: Set<AnyCancellable> = []
     
+    init(repository: RecipesRepository) {
+        self.repository = repository
+        fetchRecipes()
+    }
+    
+    private func fetchRecipes() {
+        repository.fetchRecipes()
+            .sink { _ in
+                
+            } receiveValue: { recipes in
+                self.recipes = recipes
+            }.store(in: &cancellables)
+    }
 }
