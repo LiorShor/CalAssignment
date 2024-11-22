e12a5a08f8b4ff7b0861f10de6d2da8778945dfb
Lior Shor <LiorShor123@gmail.com>
2024-11-22 10:33:40 +0200

    Init

---
+++ CalAssignment/CalAssignment/CalAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalAssignmentApp.swift
+//  CalAssignment
+//
+//  Created by Lior Shor on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
