d910aebfb7e94fd917ea8b2a2addef54ee72143f
Lior Shor <LiorShor123@gmail.com>
2024-11-22 15:56:59 +0200

    Cleaned up the view and added logic to RecipeDetailsViewModel

--- CalAssignment/CalAssignment/Modules/Recipes/RecipeDetails/RecipeDetails.swift
+++ CalAssignment/CalAssignment/Modules/Recipes/RecipeDetails/RecipeDetails.swift
@@ -8,41 +8,53 @@
 import SwiftUI
 
 struct RecipeDetails: View {
-    var recipe: Recipe?
+    @ObservedObject var viewModel: RecipeDetailsViewModel
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                AsyncImage(url: recipe?.imageURL) { image in
-                    image.resizable()
-                        .scaledToFill()
-                } placeholder: {
-                    ProgressView()
-                        .controlSize(.large)
-                }
-                .frame(height: geometry.size.height * 0.333)
-                .clipped() // Make sure the image doesn't overflow
-                .overlay(
-                    LinearGradient(
-                        gradient: Gradient(colors: [Color.white.opacity(0.6), Color.clear]),
-                        startPoint: .bottom,
-                        endPoint: .top
-                    )
+        // Request biometric authentication and decrypt
+        switch viewModel.authenticationStatus {
+        case .authenticating:
+            Text("Authenticating...")
+                .font(.body)
+                .padding()
+        case .authenticated:
+            GeometryReader { geometry in
+                VStack {
+                    AsyncImage(url: viewModel.recipe?.imageURL) { image in
+                        image.resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                            .controlSize(.large)
+                    }
                     .frame(height: geometry.size.height * 0.333)
-                )
-                VStack(alignment: .leading) {
-                    Text(recipe?.name ?? .empty)
-                        .font(.title)
-                    Text("Fats: \(recipe?.fats ?? .empty) | Calories: \(recipe?.calories ?? .empty) | Carbos: \(recipe?.carbos ?? .empty)")
-                        .font(.footnote)
-                    Text(recipe?.description ?? .empty)
-                        .font(.body)
-                }.padding(.horizontal)
-            }.ignoresSafeArea()
+                    .clipped() // Make sure image does not overflow
+                    .overlay(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.white.opacity(0.6), Color.clear]),
+                            startPoint: .bottom,
+                            endPoint: .top
+                        )
+                        .frame(height: geometry.size.height * 0.333)
+                    )
+                    VStack(alignment: .leading) {
+                        Text(viewModel.recipe?.name ?? .empty)
+                            .font(.title)
+                        Text("Fats: \(viewModel.recipe?.fats ?? "") | Calories: \(viewModel.recipe?.calories ?? "") | Carbos: \(viewModel.recipe?.carbos ?? "")")
+                            .font(.footnote)
+                        Text(viewModel.recipe?.description ?? .empty)
+                            .font(.body)
+                    }.padding(.horizontal)
+                }.ignoresSafeArea()
+            }
+        case .failure:
+            Text("Failure")
+                .font(.body)
+                .padding()
         }
     }
 }
 
 #Preview {
-    RecipeDetails(recipe: Recipe(id: "1", name: "Name", calories: "2 grams", carbos: "3 grams", description: "This is a description", headline: "headline", proteins: "4 grams", time: "PT35M", fats: "6 grams", difficulty: 2, imageURL: URL(string: "https://img.hellofresh.com/f_auto,q_auto/hellofresh_s3/image/533143aaff604d567f8b4571.jpg")!, thumbURL: URL(string: "https://img.hellofresh.com/f_auto,q_auto,w_300/hellofresh_s3/image/533143aaff604d567f8b4571.jpg")!))
+    RecipeDetails(viewModel: RecipeDetailsViewModel(encryptedRecipe: CryptoHelper.encrypt(recipe: Recipe(id: "1", name: "Name", calories: "2 grams", carbos: "3 grams", description: "This is a description", headline: "headline", proteins: "4 grams", time: "PT35M", fats: "6 grams", difficulty: 2, imageURL: URL(string: "https://img.hellofresh.com/f_auto,q_auto/hellofresh_s3/image/533143aaff604d567f8b4571.jpg")!, thumbURL: URL(string: "https://img.hellofresh.com/f_auto,q_auto,w_300/hellofresh_s3/image/533143aaff604d567f8b4571.jpg")!)) ?? .empty))
 }
--- CalAssignment/CalAssignment/Modules/Recipes/RecipeDetails/RecipeDetailsViewModel.swift
+++ CalAssignment/CalAssignment/Modules/Recipes/RecipeDetails/RecipeDetailsViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import LocalAuthentication
+
+enum AuthenticationStatus {
+    case authenticating
+    case authenticated
+    case failure
+}
+
+class RecipeDetailsViewModel: ObservableObject {
+    
+    var encryptedRecipe: String
+    @Published var authenticationStatus: AuthenticationStatus = .authenticating
+    @Published var recipe: Recipe?
+    
+    init(encryptedRecipe: String) {
+        self.encryptedRecipe = encryptedRecipe
+        authenticateAndDecrypt()
+    }
+    
+    func authenticateAndDecrypt() {
+        BiometricHelper.authenticate(reason: "Authenticate to view the recipe details") { success in
+            if success {
+                // Decrypt the entire recipe after successful authentication
+                if let encryptedRecipe = CryptoHelper.decrypt(encryptedRecipe: self.encryptedRecipe) {
+                    self.recipe = encryptedRecipe
+                    self.authenticationStatus = .authenticated
+                }
+            } else {
+                self.authenticationStatus = .failure
+            }
+        }
+    }
+}
