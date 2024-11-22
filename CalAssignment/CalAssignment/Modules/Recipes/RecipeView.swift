012c550318b99b62c9ddc2992785a007ea186236
Lior Shor <LiorShor123@gmail.com>
2024-11-22 11:51:33 +0200

    Added view for recipe with preview

--- CalAssignment/CalAssignment/Modules/Recipes/RecipeView.swift
+++ CalAssignment/CalAssignment/Modules/Recipes/RecipeView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct RecipeView: View {
+    var url: URL
+    var name: String
+    var fatsAmount: String
+    var calories: String
+    var carbons: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            AsyncImage(url: url) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+                    .controlSize(.large)
+            }
+            .frame(width: 150, height: 100)
+            .clipShape(
+                .rect(
+                    topLeadingRadius: 10,
+                    bottomLeadingRadius: 0,
+                    bottomTrailingRadius: 0,
+                    topTrailingRadius: 10
+                )
+            )
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title)
+                VStack(alignment: .leading) {
+                    Text("Calories: \(calories)")
+                    Text("Fats: \(fatsAmount)")
+                    Text("Carbons: \(carbons)")
+                }
+                .font(.footnote)
+            }
+        }.overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.black, lineWidth: 1)
+        )
     }
 }
 
 #Preview {
-    RecipeView()
+    RecipeView(url: URL(string: "https://img.hellofresh.com/f_auto,q_auto,w_300/hellofresh_s3/image/533143aaff604d567f8b4571.jpg")!, name: "Title", fatsAmount: "2 grams", calories: "26 grams", carbons: "47 grams")
 }
