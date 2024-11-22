d1c7fd4fa3988d3a0afb754015856c3e45b29269
Lior Shor <LiorShor123@gmail.com>
2024-11-22 11:11:14 +0200

    Added RecipeView

---
+++ CalAssignment/CalAssignment/Modules/Recipes/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  CalAssignment
+//
+//  Created by Lior Shor on 22/11/2024.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
